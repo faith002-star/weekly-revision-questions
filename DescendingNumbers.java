a342ff93a4d9831a7acdb596b1f95b4bda5ffb78
faith002-star <faey36312@gmail.com>
2026-02-24 18:45:35 +0300

    Add files via upload

---
+++ DescendingNumbers.java
@@ -0,0 +1,10 @@
+public class DescendingNumbers {
+    public static void main(String[] args) {
+        
+        // Loop starts at 100, continues while i >= 50, and subtracts 1 each time
+        for (int i = 100; i >= 50; i--) {
+            System.out.println(i);
+        }
+        
+    }
+}
